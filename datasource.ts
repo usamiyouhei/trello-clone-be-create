fab7338021dc2184ec2f51196e0824e1ff231652
youhei usami <usao9110@icloud.com>
2026-02-23 11:51:35 +0400

    2/23

---
+++ datasource.ts
@@ -0,0 +1,10 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "sqlite",
+  database: "trello-clone.sqlite",
+  synchronize: true,
+  logging: false,
+  entities: [],
+  migrations: [],
+});
