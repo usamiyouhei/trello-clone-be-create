bdb15aab6324a30654ef4bfbf86c67d594a8758f
youhei usami <usao9110@icloud.com>
2026-02-25 12:18:52 +0400

    2/25

---
+++ entities/list.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Card } from "./card.entity";
+
+@Entity()
+export class List {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  position!: number;
+  @OneToMany(() => Card, (card) => card.list, { cascade: true })
+  cards?: Card[];
+
+  @CreateDateColumn()
+  readonly createdAt?: Date;
+
+  @UpdateDateColumn()
+  readonly updatedAt?: Date;
+}
